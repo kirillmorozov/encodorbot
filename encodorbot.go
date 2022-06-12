2471a23ce1fecc6ecaed003b77f6b872ca858c86
Kirill Morozov <kirill@robotix.pro>
2022-06-12 22:54:33 +0300

    docs: Add docstrings

--- encodorbot.go
+++ encodorbot.go
@@ -15,26 +15,35 @@ import (
 )
 
 const (
+	// tokenEnvVar is an environment variable that contains telegram bot token.
 	tokenEnvVar = "TELEGRAM_BOT_TOKEN"
 )
 
 const (
-	startCommand     = "/start"
+	// startCommand is the first command that's send from the new user.
+	startCommand = "/start"
+	// beghiloszCommand is the command that's used to encode messages using
+	// calculator spelling.
 	beghiloszCommand = "/beghilosz"
-	zalgoCommand     = "/zalgo"
+	// zalgoCommand is the command that's used to encode messages zalgo.
+	zalgoCommand = "/zalgo"
 )
 
 const (
+	// botUsage is format that explains bot usage.
 	botUsage = `Usage:
 	[command] your message
 
 Available Commands:
 	%v – Encode your message using calculator spelling
 	%v – Encode your message using zalgo`
+	// beghiloszUsage is a format string that expains beghiloszCommand usage.
 	beghiloszUsage = "To encode your message using calculator spelling send `%v YOUR MESSAGE`"
-	zalgoUsage     = "To encode your message using zalgo send `%v YOUR MESSAGE`"
+	// zalgoUsage is a format string that expains zalgoCommand usage.
+	zalgoUsage = "To encode your message using zalgo send `%v YOUR MESSAGE`"
 )
 
+// newBot returns a configured telegram bot.
 func newBot() *telebot.Bot {
 	settings := telebot.Settings{
 		Token:     os.Getenv(tokenEnvVar),
@@ -51,10 +60,12 @@ func newBot() *telebot.Bot {
 	return bot
 }
 
+// handleStart handles startCommand messages.
 func handleStart(c telebot.Context) error {
 	return c.Reply(fmt.Sprintf(botUsage, beghiloszCommand, zalgoCommand))
 }
 
+// handleBeghilosz handles beghiloszCommand messages.
 func handleBeghilosz(c telebot.Context) error {
 	if c.Message().Payload == "" {
 		usage := fmt.Sprintf(beghiloszUsage, beghiloszCommand)
@@ -64,6 +75,7 @@ func handleBeghilosz(c telebot.Context) error {
 	return c.Reply(encodedText)
 }
 
+// handleZalgo handles zalgoCommand messages.
 func handleZalgo(c telebot.Context) error {
 	if c.Message().Payload == "" {
 		usage := fmt.Sprintf(zalgoUsage, zalgoCommand)
@@ -76,6 +88,7 @@ func handleZalgo(c telebot.Context) error {
 	return c.Reply(encodedText)
 }
 
+// handleText handles all plain text messages.
 func handleText(c telebot.Context) error {
 	encodorCmd := cmd.NewRoot()
 	var output strings.Builder
@@ -87,12 +100,16 @@ func handleText(c telebot.Context) error {
 	return c.Reply(output.String())
 }
 
+// HandleTelegramWebHook is the cloud function entry point.
+//
+// It decodes a telebot.Update from the http.Request, creates a pre-configured
+// telebot.Bot and processes the update through this bot.
 func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
-	bot := newBot()
 	var update telebot.Update
 	if decodeErr := json.NewDecoder(r.Body).Decode(&update); decodeErr != nil {
 		log.Print(decodeErr)
 		return
 	}
+	bot := newBot()
 	bot.ProcessUpdate(update)
 }
