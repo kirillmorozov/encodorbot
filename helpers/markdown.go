9ea43a778cce8907ab86154624181cc4abc2afaa
Kirill Morozov <kirill@robotix.pro>
2022-06-18 16:08:36 +0300

    feat: Add `escapeMarkdownV2` function

--- helpers/markdown.go
+++ helpers/markdown.go
@@ -1,10 +1,22 @@
 package helpers
 
 import (
+	"regexp"
+
 	"gopkg.in/telebot.v3"
 )
 
 // escapeMarkdownV2 escapes telegram markup symbols.
 func escapeMarkdownV2(text string, entityType telebot.EntityType) string {
-	return ""
+	var escapeChars string
+	switch entityType {
+	case telebot.EntityCode, telebot.EntityCodeBlock:
+		escapeChars = "\\`"
+	case telebot.EntityTextLink:
+		escapeChars = "\\)"
+	default:
+		escapeChars = "\\_*[]()~`>#+-=|{}.!"
+	}
+	re := regexp.MustCompilePOSIX("([{" + regexp.QuoteMeta(escapeChars) + "}])")
+	return re.ReplaceAllString(text, "\\$1")
 }
