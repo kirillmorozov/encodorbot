f2236eeef7322f23495e826a3cb9b09278ae903e
Kirill Morozov <kirill@robotix.pro>
2022-06-18 14:57:34 +0300

    test: Add tests for `escapeMarkdownV2`

---
+++ helpers/markdown.go
@@ -0,0 +1,10 @@
+package helpers
+
+import (
+	"gopkg.in/telebot.v3"
+)
+
+// escapeMarkdownV2 escapes telegram markup symbols.
+func escapeMarkdownV2(text string, entityType telebot.EntityType) string {
+	return ""
+}
---
+++ helpers/markdown_test.go
@@ -0,0 +1,61 @@
+package helpers
+
+import (
+	"testing"
+
+	"gopkg.in/telebot.v3"
+)
+
+func Test_escapeMarkdownV2(t *testing.T) {
+	type args struct {
+		text       string
+		entityType telebot.EntityType
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty text",
+			args: args{text: ``},
+			want: ``,
+		},
+		{
+			name: "Escape markdown v2",
+			args: args{text: `a_b*c[d]e (fg) h~I` + "`" + `>JK#L+MN -O=|p{qr}s.t!\ \u`},
+			want: `a\_b\*c\[d\]e \(fg\) h\~I\` + "`" + `\>JK\#L\+MN \-O\=\|p\{qr\}s\.t\!\\ \\u`,
+		},
+		{
+			name: "Monospaced code block",
+			args: args{
+				text:       `mono/pre: ` + "`" + `abc` + "`" + ` \int (` + "`" + `\some \` + "`" + `stuff)`,
+				entityType: telebot.EntityCodeBlock,
+			},
+			want: `mono/pre: \` + "`" + `abc\` + "`" + ` \\int (\` + "`" + `\\some \\\` + "`" + `stuff)`,
+		},
+		{
+			name: "Monospaced code",
+			args: args{
+				text:       `mono/pre: ` + "`" + `abc` + "`" + ` \int (` + "`" + `\some \` + "`" + `stuff)`,
+				entityType: telebot.EntityCode,
+			},
+			want: `mono/pre: \` + "`" + `abc\` + "`" + ` \\int (\` + "`" + `\\some \\\` + "`" + `stuff)`,
+		},
+		{
+			name: "Text link",
+			args: args{
+				text:       `https://url.containing/funny)cha)\\ra\\)cter\\s`,
+				entityType: telebot.EntityTextLink,
+			},
+			want: `https://url.containing/funny\)cha\)\\\\ra\\\\\)cter\\\\s`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := escapeMarkdownV2(tt.args.text, tt.args.entityType); got != tt.want {
+				t.Errorf("escapeMarkdownV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
